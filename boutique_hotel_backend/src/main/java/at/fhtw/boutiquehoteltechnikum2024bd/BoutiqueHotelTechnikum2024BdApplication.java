18f4f02ebc952aba8676526bc191b0d6e5c4f4ef
Lobna Boutahar <Lobna.Boutahar-CIC@ibm.com>
2024-05-21 21:17:18 +0200

    create spring boot project

---
+++ boutique_hotel_backend/src/main/java/at/fhtw/boutiquehoteltechnikum2024bd/BoutiqueHotelTechnikum2024BdApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.boutiquehoteltechnikum2024bd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueHotelTechnikum2024BdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueHotelTechnikum2024BdApplication.class, args);
+	}
+
+}
---
+++ boutique_hotel_backend/src/test/java/at/fhtw/boutiquehoteltechnikum2024bd/BoutiqueHotelTechnikum2024BdApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.boutiquehoteltechnikum2024bd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoutiqueHotelTechnikum2024BdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
